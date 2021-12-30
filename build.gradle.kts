41ca949b1465b3efa172ba44d8af6d46015d2231
LaoLittle <y1941803875@outlook.com>
2021-12-30 22:02:23 +0800

    add implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,3 +13,12 @@ repositories {
     maven("https://maven.aliyun.com/repository/public")
     mavenCentral()
 }
+
+dependencies {
+    val exposedVersion = "0.37.1"
+    implementation("org.xerial:sqlite-jdbc:3.36.0.2")
+    implementation("com.alibaba:druid:1.2.8")
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+}
