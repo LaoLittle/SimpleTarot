71ce39311ed70f6dbb0290e642fca2d8066cd449
LaoLittle <y1941803875@outlook.com>
2021-12-30 22:02:32 +0800

    update

---
+++ src/main/kotlin/Database.kt
@@ -0,0 +1,19 @@
+package org.laolittle.plugin
+
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+
+object Database {
+
+
+    object User: Table(){
+        val id = long("id").uniqueIndex()
+        val card = integer("cards")
+        val date = integer("date")
+    }
+
+    suspend inline fun <T> suspendTransaction(crossinline block: suspend Transaction.() -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+}
--- src/main/kotlin/Tarot.kt
+++ src/main/kotlin/Tarot.kt
@@ -1,14 +1,26 @@
 package org.laolittle.plugin
 
+import com.alibaba.druid.pool.DruidDataSource
 import kotlinx.coroutines.delay
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.contact.Contact
+import net.mamoe.mirai.contact.User
 import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.subscribeGroupMessages
 import net.mamoe.mirai.message.code.MiraiCode.deserializeMiraiCode
-import net.mamoe.mirai.message.data.*
+import net.mamoe.mirai.message.data.Image
+import net.mamoe.mirai.message.data.Message
+import net.mamoe.mirai.message.data.buildForwardMessage
 import net.mamoe.mirai.utils.ExternalResource.Companion.uploadAsImage
 import net.mamoe.mirai.utils.info
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.TransactionManager
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.io.File
+import java.sql.Connection
+import java.time.LocalDate
+import javax.sql.DataSource
 
 object Tarot : KotlinPlugin(
     JvmPluginDescription(
@@ -19,14 +31,29 @@ object Tarot : KotlinPlugin(
         author("LaoLittle")
     }
 ) {
+    private val path: File = dataFolder.resolve("TarotImages")
+    private val userDB: Database.User
+    private val dataSource = DruidDataSource()
+    private val db: org.jetbrains.exposed.sql.Database
 
     override fun onEnable() {
-       TarotConfig.reload()
+        TarotConfig.reload()
         TarotData.reload()
         logger.info { "Plugin loaded" }
 
         GlobalEventChannel.subscribeGroupMessages {
+            val msg: (TarotData.Tarot, User) -> Message = { tarot, target ->
+                TarotConfig.format.replace("%目标%", "[mirai:at:${target.id}]")
+                    .replace("%牌名%", tarot.name).replace(
+                        "%描述%",
+                        if ((0..1).random() == 0) "正位\n${tarot.positive}" else "逆位\n${tarot.negative}"
+                    ).deserializeMiraiCode()
+            }
+            val img: suspend (TarotData.Tarot, Contact) -> Image = { tarot, contact ->
+                path.resolve(tarot.imageName).uploadAsImage(contact)
+            }
             finding(Regex("^(?:(.+)张|)塔罗牌")) {
+                val sql: SqlExpressionBuilder.() -> Op<Boolean> = { userDB.id eq sender.id }
                 val tarotNum = when (val value = it.groups[1]?.value) {
                     "一", "壹", "单", null -> 1
                     "二", "贰", "两" -> 2
@@ -40,39 +67,105 @@ object Tarot : KotlinPlugin(
                     "十", "拾" -> 10
                     else -> value.toIntOrNull() ?: return@finding
                 }
+                val la = Database.suspendTransaction {
+                    val cardRow = userDB.select(sql).singleOrNull() ?: return@suspendTransaction null
+                    val cards = cardRow[userDB.card]
+                    if (cardRow[userDB.card] >= tarotNum)
+                        userDB.update(sql) { info ->
+                            info[card] = cards - tarotNum
+                        } else {
+                        if (cards == 0) subject.sendMessage("你没有抽数了")
+                        else subject.sendMessage("你只有${cardRow[userDB.card]}张")
+                        return@suspendTransaction false
+                    }
+                    true
+                }
+                when (la) {
+                    null -> {
+                        subject.sendMessage("请发送\"今日塔罗\"来获取塔罗牌次数");return@finding
+                    }
+                    false -> return@finding
+                    true -> delay(500)
+                }
                 if (tarotNum in 1..10) {
-                    val msg: (TarotData.Tarot) -> Message = { tarot ->
-                        TarotConfig.format.replace("%目标%", "[mirai:at:${sender.id}]")
-                            .replace("%牌名%", tarot.name).replace("%描述%",
-                                if ((0..1).random() == 0) "正位\n${tarot.positive}" else "逆位\n${tarot.negative}"
-                            ).deserializeMiraiCode()
-                    }
-                    val img: suspend (TarotData.Tarot) -> Image = { tarot ->
-                        dataFolder.resolve(tarot.imageName).uploadAsImage(subject)
-                    }
                     if (tarotNum == 1) {
                         val card = TarotData.tarot.random()
-                        subject.sendMessage(msg(card))
+                        subject.sendMessage(msg(card, sender))
                         delay(TarotConfig.interval)
-                        subject.sendMessage(img(card))
+                        subject.sendMessage(img(card, subject))
                     } else {
-                        val tarots = mutableSetOf<TarotData.Tarot>()
-                        while (tarots.size < tarotNum) {
-                            tarots.add(TarotData.tarot.random())
-                            if (tarots.size == TarotData.tarot.size) break
-                        }
+                        val tarots = getRandomTarots(tarotNum)
                         val forward = buildForwardMessage {
                             tarots.forEach { tarot ->
-                                add(sender, msg(tarot))
-                                add(sender, img(tarot))
+                                add(sender, msg(tarot, sender))
+                                add(sender, img(tarot, subject))
                             }
                         }
                         subject.sendMessage(forward)
                     }
-                }else {
+                } else {
                     subject.sendMessage("次数请限制在一到十内！")
                 }
             }
+            startsWith("今日塔罗") {
+                val sql: SqlExpressionBuilder.() -> Op<Boolean> = { userDB.id eq sender.id }
+                val dayOfYear = LocalDate.now().dayOfYear
+                Database.suspendTransaction {
+                    val query = userDB.select(sql)
+                    val user = query.singleOrNull()
+                    if (user?.get(userDB.date) == dayOfYear) {
+                        subject.sendMessage("你已经拿过一张今日塔罗了哦！")
+                        return@suspendTransaction
+                    }
+                    val random = (10..30).random()
+                    if (user == null)
+                        userDB.insert { usr ->
+                            usr[id] = sender.id
+                            usr[card] = random
+                            usr[date] = dayOfYear
+                        }
+                    else {
+                        userDB.update(sql) { info ->
+                            info[card] = 1
+                            info[date] = dayOfYear
+                        }
+                    }
+                    val card = TarotData.tarot.random()
+                    subject.sendMessage(msg(card, sender))
+                    delay(TarotConfig.interval)
+                    subject.sendMessage(img(card, subject))
+                    subject.sendMessage("获得$random 张塔罗牌")
+                }
+            }
+            startsWith("查看塔罗") {
+
+            }
+        }
+
+    }
+
+    private fun getRandomTarots(tarotNum: Int): Set<TarotData.Tarot> {
+        val tarots = mutableSetOf<TarotData.Tarot>()
+        if (TarotConfig.repeatable) while (tarots.size < tarotNum) {
+            tarots.add(TarotData.tarot.random())
+            if (tarots.size == TarotData.tarot.size) break
+        } else {
+            repeat(tarotNum) {
+                tarots.add(TarotData.tarot.random())
+            }
+        }
+        return tarots
+    }
+
+    init {
+        dataSource.url = "jdbc:sqlite:${Tarot.dataFolder}/${TarotConfig.database}"
+        dataSource.driverClassName = "org.sqlite.JDBC"
+        db = org.jetbrains.exposed.sql.Database.connect(dataSource as DataSource)
+        TransactionManager.manager.defaultIsolationLevel =
+            Connection.TRANSACTION_SERIALIZABLE
+        userDB = Database.User
+        transaction {
+            SchemaUtils.create(userDB)
         }
     }
 }
