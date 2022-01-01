a46046f28138a10fb4119bfa8f1a3d213fee8fb8
LaoLittle <y1941803875@outlook.com>
2022-01-01 18:11:54 +0800

    update

--- src/main/kotlin/GetTarot.kt
+++ src/main/kotlin/GetTarot.kt
@@ -5,8 +5,12 @@ import net.mamoe.mirai.message.code.MiraiCode.deserializeMiraiCode
 import net.mamoe.mirai.message.data.Message
 import net.mamoe.mirai.utils.ExternalResource
 import net.mamoe.mirai.utils.ExternalResource.Companion.toExternalResource
-import org.laolittle.plugin.Tools.rotateImage
+import java.awt.Graphics2D
+import java.awt.RenderingHints
+import java.awt.image.BufferedImage
+import java.io.ByteArrayOutputStream
 import java.io.File
+import javax.imageio.ImageIO
 
 class GetTarot(
     private val tarot: TarotData.Tarot,
@@ -33,4 +37,22 @@ class GetTarot(
                     imgFile.rotateImage()
             } else null
         }
+
+    companion object {
+        private fun File.rotateImage(): ExternalResource {
+            val buffImage = ImageIO.read(this)
+            val w = buffImage.width
+            val h = buffImage.height
+            val type = buffImage.colorModel.transparency
+            val img = BufferedImage(w, h, type)
+            val g2: Graphics2D = img.createGraphics()
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR)
+            g2.rotate(Math.toRadians(180.0), (w shr 1).toDouble(), (h shr 1).toDouble())
+            g2.drawImage(buffImage, 0, 0, null)
+            g2.dispose()
+            val bytes = ByteArrayOutputStream()
+            ImageIO.write(img, "jpg", bytes)
+            return bytes.toByteArray().toExternalResource()
+        }
+    }
 }
